9a01dca869adfeaadc349934334cee79da8bf127
nuno <al75588@alunos.utad.pt>
2022-06-16 12:06:09 +0100

    Sala Manager Adicionados os Metodos

--- ProjetoUbiqua/EntitiesManagers/SalaManager.cs
+++ ProjetoUbiqua/EntitiesManagers/SalaManager.cs
@@ -14,7 +14,7 @@ namespace ProjetoUbiqua.EntitiesManagers
             _dataContext = dataContext;
         }
 
-        public async Task<Sala> Adicionar(Sala sala)
+        public async Task<Sala> AdicionarSala(Sala sala)
         {
             sala.ID_Sala = 0;
             await _dataContext.AddAsync(sala);
@@ -23,13 +23,92 @@ namespace ProjetoUbiqua.EntitiesManagers
             return sala;
         }
 
-        public async Task Editar(Sala sala)
+        public async Task EditarSala(Sala sala)
         {
             if (sala == default)
                 throw new NullReferenceException();
 
             _dataContext.Update(sala);
             await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task<Sala> VisualizarSala(int IdSala)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).Include(x => x.Sensores).FirstOrDefaultAsync();
+
+            if (sala == default)
+                throw new NullReferenceException();
+
+            return sala;
+        }
+
+        public async Task ApagarSala(int IdSala)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).FirstOrDefaultAsync();
+
+            if (sala == default)
+                throw new NullReferenceException();
+
+            _dataContext.Sala.Remove(sala);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task AssociarSensorSala(int IdSensor, int IdSala)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).Include(x => x.Sensores).FirstOrDefaultAsync();
+            var sensor = await _dataContext.Sensor.Where(sensor => sensor.ID_Sensor == IdSensor).FirstOrDefaultAsync();
+
+            if (sala == default || sensor == default)
+                throw new NullReferenceException();
+
+            if (sala.Sensores != default && sala.Sensores.Any(sensor => sensor.ID_Sensor == IdSensor))
+                return;
+
+            sala.Sensores.Add(sensor);
+
+            _dataContext.Update(sala);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task DesassociarSensorSala(int IdSensor, int IdSala)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).Include(x => x.Sensores).FirstOrDefaultAsync();
+
+            if (sala == default)
+                throw new NullReferenceException();
+
+            sala.Sensores = sala.Sensores.Where(sensor => sensor.ID_Sensor != IdSensor).ToList();
+
+            _dataContext.Update(sala);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task LigarDesligarLuzes(int IdSala,bool Estado)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).Include(x => x.Sensores).FirstOrDefaultAsync();
+
+            if (sala == default)
+                throw new NullReferenceException();
+
+            sala.EstadoLuzes = Estado;
+
+            _dataContext.Update(sala);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Sensor>> GetListSensores(int IdSala)
+        {
+            var sala = await _dataContext.Sala.Where(sala => sala.ID_Sala == IdSala).Include(x => x.Sensores).FirstOrDefaultAsync();
+
+            if (sala == default)
+                throw new NullReferenceException();
+
+            if (sala.Sensores == null)
+                throw new NullReferenceException();
+
+            List<Sensor> sensores = sala.Sensores;
+
+            return sensores;
         }
     }
 }
