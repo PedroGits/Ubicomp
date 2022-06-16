b72b3237b72adc628819c136c42449813a914245
nuno <al75588@alunos.utad.pt>
2022-06-16 11:26:18 +0100

    Add Sala e Editar Sala adicionados

--- ProjetoUbiqua/EntitiesManagers/SalaManager.cs
+++ ProjetoUbiqua/EntitiesManagers/SalaManager.cs
@@ -1,8 +1,35 @@
-﻿using ProjetoUbiqua.EntitiesManagers.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoUbiqua.Context;
+using ProjetoUbiqua.Entities;
+using ProjetoUbiqua.EntitiesManagers.Interfaces;
 
 namespace ProjetoUbiqua.EntitiesManagers
 {
     public class SalaManager:ISalaManager
     {
+        private readonly DataContext _dataContext;
+
+        public SalaManager(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<Sala> Adicionar(Sala sala)
+        {
+            sala.ID_Sala = 0;
+            await _dataContext.AddAsync(sala);
+            await _dataContext.SaveChangesAsync();
+
+            return sala;
+        }
+
+        public async Task Editar(Sala sala)
+        {
+            if (sala == default)
+                throw new NullReferenceException();
+
+            _dataContext.Update(sala);
+            await _dataContext.SaveChangesAsync();
+        }
     }
 }
